50117be4eb4711d567381215a9b494138764c00e
ngocthanh1009 <tngocthanh2004@gmail.com>
2025-07-20 12:06:28 +0700

    updateanhr

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.use(function(err, req, res, next) {
 });
 
 //4. config port (for cloud deployment)
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
